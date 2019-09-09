49fb735709f52798b5c7cc3b136cbd1a11c820e2
XIngmingQu <quxingming@qq.com>
2019-09-09 15:55:31 -0500

    add dark model

--- SMUExampleOne/BrandAndPickerViewController.m
+++ SMUExampleOne/BrandAndPickerViewController.m
@@ -9,9 +9,12 @@
 #import "BrandAndPickerViewController.h"
 
 @interface BrandAndPickerViewController ()
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControlButton;
+//@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControlButton;
 @property (weak, nonatomic) IBOutlet UITextView *historyText;
 @property (weak, nonatomic) IBOutlet UIImageView *brandView;
+@property (weak, nonatomic) IBOutlet UISwitch *SwitchButton;
+@property (weak, nonatomic) IBOutlet UILabel *DarkModelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
 
@@ -20,34 +23,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-//    _historyText.text = @"HisroryHisroryHisroryHisroryHisrory";
+    //    _historyText.text = @"HisroryHisroryHisroryHisroryHisrory";
 }
-- (IBAction)SegIndexChanged:(UISegmentedControl *)sender {
-    switch (sender.selectedSegmentIndex) {
-    case 0:
-//        NSLog(@"@000");
-            self.view.backgroundColor = [UIColor whiteColor];
-            _historyText.backgroundColor =[UIColor whiteColor];
-            _brandView.backgroundColor =[UIColor whiteColor];
-            break;
-    case 1:
-//        NSLog(@"@1111");
-            self.view.backgroundColor = [UIColor lightGrayColor];
-            _historyText.backgroundColor = [UIColor lightGrayColor];
-            _brandView.backgroundColor =[UIColor lightGrayColor];
-            break;
+
+- (IBAction)SwitchChanged:(UISwitch *)sender {
+    if (sender.on) {
+        self.view.backgroundColor = [UIColor lightGrayColor];
+        _historyText.backgroundColor = [UIColor lightGrayColor];
+        _brandView.backgroundColor =[UIColor lightGrayColor];
+        
+        _DarkModelLabel.textColor = [UIColor whiteColor];
+        _historyText.textColor = [UIColor whiteColor];
+        _titleLabel.textColor = [UIColor whiteColor];
+    }else{
+        self.view.backgroundColor = [UIColor whiteColor];
+        _historyText.backgroundColor =[UIColor whiteColor];
+        _brandView.backgroundColor =[UIColor whiteColor];
+        
+        _DarkModelLabel.textColor = [UIColor blackColor];
+        _historyText.textColor = [UIColor blackColor];
+        _titleLabel.textColor = [UIColor blackColor];
     }
-    
 }
+
+//- (IBAction)SegIndexChanged:(UISegmentedControl *)sender {
+//    switch (sender.selectedSegmentIndex) {
+//    case 0:
+////        NSLog(@"@000");
+//            self.view.backgroundColor = [UIColor whiteColor];
+//            _historyText.backgroundColor =[UIColor whiteColor];
+//            _brandView.backgroundColor =[UIColor whiteColor];
+//            break;
+//    case 1:
+////        NSLog(@"@1111");
+//            self.view.backgroundColor = [UIColor lightGrayColor];
+//            _historyText.backgroundColor = [UIColor lightGrayColor];
+//            _brandView.backgroundColor =[UIColor lightGrayColor];
+//            break;
+//    }
+//
+//}
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
